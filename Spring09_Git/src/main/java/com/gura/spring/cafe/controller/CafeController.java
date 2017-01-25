a62ac5e3f12ae37821bbb4403c69c47eab5ef2c6
parkhyoungju <qkrgudwn78@gmail.com>
2017-01-25 12:26:31 +0900

    /cafe/private/* 요청에 aop 를 이용한 인증 구현

--- Spring09_Git/src/main/java/com/gura/spring/cafe/controller/CafeController.java
+++ Spring09_Git/src/main/java/com/gura/spring/cafe/controller/CafeController.java
@@ -32,15 +32,16 @@ public class CafeController {
 	}
 	
 	@RequestMapping("/cafe/private/insertform")
-	public String insertForm(){
-		return "cafe/private/insertform";
+	public ModelAndView authInsertForm(){
+		
+		return new ModelAndView("cafe/private/insertform");
 	}
 	
 	@RequestMapping("/cafe/private/insert")
-	public String insert(@ModelAttribute CafeDto dto){
+	public ModelAndView authInsert(@ModelAttribute CafeDto dto){
 		cafeService.insert(dto);
 		
-		return "redirect:/cafe/list.do";
+		return new ModelAndView("redirect:/cafe/list.do");
 	}
 	
 	@RequestMapping("/cafe/detail")
@@ -52,14 +53,14 @@ public class CafeController {
 	}
 	
 	@RequestMapping("/cafe/private/delete")
-	public String delete(@RequestParam int num){
+	public ModelAndView authDelete(@RequestParam int num){
 		cafeService.delete(num);
 		
-		return "redirect:/cafe/list.do";
+		return new ModelAndView("redirect:/cafe/list.do");
 	}
 	
 	@RequestMapping("/cafe/private/updateform")
-	public ModelAndView updateform(@RequestParam int num){
+	public ModelAndView authUpdateform(@RequestParam int num){
 		ModelAndView mView = cafeService.updateForm(num);
 		mView.setViewName("cafe/private/updateform");
 		
@@ -67,9 +68,9 @@ public class CafeController {
 	}
 	
 	@RequestMapping("/cafe/private/update")
-	public String update(@ModelAttribute CafeDto dto){
+	public ModelAndView authUpdate(@ModelAttribute CafeDto dto){
 		cafeService.update(dto);
 		
-		return "redirect:/cafe/list.do";
+		return new ModelAndView("redirect:/cafe/list.do");
 	}
 }
